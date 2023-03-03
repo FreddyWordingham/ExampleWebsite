4c3ec62eb4325e9351c08fdd420af9a46bbf68cf
FreddyWordingham <freddy@digilab.co.uk>
2023-03-03 14:11:26 +0000

    Tweak

--- api/main.py
+++ api/main.py
@@ -1,11 +1,23 @@
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
 
 from . import settings
 
+
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -16,3 +28,12 @@ async def homepage(request: Request):
 @app.get("/hello/{name}")
 async def hello_name(name: str):
     return f"Hello, {name}!"
+
+
+class SampleInput(BaseModel):
+    x: int
+
+
+@app.post("/sample")
+async def sample(input: SampleInput):
+    return input.x**2
--- static/logic.js
+++ static/logic.js
@@ -1,5 +1,25 @@
-function hello() {
-    console.log("Hello World!");
-}
+function sample() {
+    const x = 3;
 
-hello();
+    const url = "http://localhost:8000/sample";
+    // const url = "http://127.0.0.1:8000/sample";
+
+    const params = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            x: x,
+        }),
+    };
+
+    fetch(url, params)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+}
